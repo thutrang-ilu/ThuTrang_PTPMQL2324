68c14ddd34d086e609474a67629706d466099db3
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-08-22 09:21:35 +0700

    Gan, lay gia tri cua doi tuong va goi phuong thuc

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,7 +3,14 @@ public class Program
 {
 	private static void Main(string[] args)
 	{
+		//Khoi tao 2 doi tuong tu class Person
 		Person ps1 = new Person();
 		Person ps2 = new Person();
+		//gan gia tri cho thuoc tinh doi tuong ps1
+		ps1.FullName = "Nguyen Van A";
+		ps1.Address = "Ha Noi";
+		ps1.Age = 18;
+		// Goi phuong thuc hien thi thong tin
+		ps1.Display();
 	}
 }
