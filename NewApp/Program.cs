18ba1b5c9978583fe80c5e48c8e345f14f3dbb85
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-08-22 10:18:48 +0700

    Student ke thua Person

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -3,23 +3,9 @@ public class Program
 {
 	private static void Main(string[] args)
 	{
-		/*
-		//Khoi tao 2 doi tuong tu class Person
-		Person ps1 = new Person();
-		Person ps2 = new Person();
-		//gan gia tri cho thuoc tinh doi tuong ps1
-		ps1.FullName = "Nguyen Van A";
-		ps1.Address = "Ha Noi";
-		ps1.Age = 18;
-		// Goi phuong thuc hien thi thong tin
-		ps1.Display();
-		ps2.Display();
-		*/
-		// Khoi tao doi tuong ps tu class Person
-		Person ps = new Person();
-		string str = "Nguyen Van A";
-		int a = 22;
-		Console.WriteLine("{0} sinh nam {1}", str, ps.GetYearOfBirth(a));
-
+		Student std = new Student();
+		std.EnterData();
+		std.StudentCode = "123456789";
+		std.Display();
 	}
 }
