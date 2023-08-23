a72896c763848c9d81069025cec264b321035bee
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-08-23 10:20:58 +0700

    Create_Demo_App

---
+++ Demo/Models/Vegetable.cs
@@ -0,0 +1,10 @@
+namespace Demo.Models
+{
+    public class Vegetable
+    {
+        public string VegetableID { get; set; }
+       public string VegetableName { get; set; }
+       public int hanSD { get; set; }
+      
+    }
+} 
---
+++ Demo/Program.cs
@@ -0,0 +1,6 @@
+﻿using Demo.Models;
+public class Program
+{
+        Vegetable veg = new Vegetable();
+        Vegetable veg01 = new Vegetable();
+}
