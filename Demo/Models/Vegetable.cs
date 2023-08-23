b31498e622b60493a8ba26cab9b4ea5b48cc5efd
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-08-23 10:27:10 +0700

    Tao them thuoc tinh cho class

--- Demo/Models/Vegetable.cs
+++ Demo/Models/Vegetable.cs
@@ -5,6 +5,8 @@ namespace Demo.Models
         public string VegetableID { get; set; }
        public string VegetableName { get; set; }
        public int hanSD { get; set; }
+       public int Gia { get; set; }
+       public string Nguon { get; set; }
       
     }
 } 
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,4 +3,5 @@ public class Program
 {
         Vegetable veg = new Vegetable();
         Vegetable veg01 = new Vegetable();
+        Vegetable veg02 = new Vegetable();
 }
