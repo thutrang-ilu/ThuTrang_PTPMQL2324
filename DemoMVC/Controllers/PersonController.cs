08d2a4631dec2ee3f5f2836cb10d45cb5ed01a40
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-08-30 16:24:20 +0700

    Tinh luong cho nhan vien

--- DemoMVC/Controllers/PersonController.cs
+++ DemoMVC/Controllers/PersonController.cs
@@ -48,6 +48,22 @@ public class PersonController : Controller
         ViewBag.gpt = ketqua;
         return View();
     }
-
+    public IActionResult Tinhluong()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Tinhluong(string Luongcb, string heSo, string Phucap)
+    {
+        double cb =0, hs =0, pc =0, luong = 0;
+        string trave;
+        if(!String.IsNullOrEmpty(Luongcb)) cb = Convert.ToDouble(Luongcb);
+        if(!String.IsNullOrEmpty(heSo)) hs = Convert.ToDouble(heSo);
+        if(!String.IsNullOrEmpty(Phucap)) pc = Convert.ToDouble(Phucap);
+        luong = cb*hs+pc;
+        trave = "Lương của bạn hiện tại là: " + luong;
+        ViewBag.tl = trave;
+        return View();
+    }
 
 }
